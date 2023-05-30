59ccb55126e237ed57009a2f792d63e74fa00bf5
Bahaa <Bahaa@DESKTOP-FA8A547>
2023-05-30 14:59:26 -0700

    added a data for smash games

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-alert("Hellow world");
+const data= {
+    brandName: "Smash Games",
+        pages:[
+            {
+            names:"index",
+            pageName:"Home",
+            callToAction:{
+            ser:"images/SmashGamesLogo-short.png",
+            altText:"Smash Games Logo",
+            }
+        },
+            {
+            names:"blast",
+            pageName:"Inferno Blast",
+            callToAction:{
+            ser:"images/Inferno/Inferno-1-short.png",
+            buttonLinkSrc:"https",
+            altText:"Inferno Blast Gameplay",
+            buttonLinkSrc:"https://steampowered.com",
+            buttonLinkText:"Buy Now on Steam!"
+            },
+        },
+            {
+            names:"index",
+            pageName:"Home",
+            callToAction:{
+            ser:"images/SmashGamesLogo-short.png",
+            altText:"Smash Games Logo",
+            }
+        },
+        ] 
+}
